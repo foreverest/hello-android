8d6d3c8a782e8f1d49d65ff4d4edabe8b25a0e6c
zabe110 <zabe110@yandex.ru>
2017-04-03 19:35:32 +0400

    Some enhancements

--- app/src/main/java/com/example/foreverest/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/foreverest/helloandroid/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.foreverest.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void switchButtonOnClick(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        if (message.isEmpty()) {
+            editText.setText("Hello Android !");
+        }
+        else {
+            editText.setText("");
+        }
+
+    }
+
 }
