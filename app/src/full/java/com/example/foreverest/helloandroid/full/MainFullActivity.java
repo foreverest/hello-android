10e01fe272cd01cf6412f12279a13edcf08fa90e
foreverest <dmitry.zabello@gmail.com>
2017-04-11 17:20:50 +0400

    Add MainFullActivity

---
+++ app/src/full/java/com/example/foreverest/helloandroid/full/MainFullActivity.java
@@ -0,0 +1,15 @@
+package com.example.foreverest.helloandroid.full;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.foreverest.helloandroid.demo.custom.R;
+
+public class MainFullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_full);
+    }
+}
