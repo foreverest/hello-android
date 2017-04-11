ca9cd1304b5e6c34934218f7fc4ed1bd81647976
foreverest <dmitry.zabello@gmail.com>
2017-04-11 17:24:09 +0400

    Add MainDemoActivity

--- app/src/demo/java/com/example/foreverest/helloandroid/demo/MainDemoActivity.java
+++ app/src/demo/java/com/example/foreverest/helloandroid/demo/MainDemoActivity.java
@@ -1,15 +1,15 @@
-package com.example.foreverest.helloandroid.full;
+package com.example.foreverest.helloandroid.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 //import com.example.foreverest.helloandroid.demo.custom.R;
 
-public class MainFullActivity extends AppCompatActivity {
+public class MainDemoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main_full);
+        //setContentView(R.layout.activity_main_demo);
     }
 }
