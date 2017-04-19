da65d33cb3c34a368cc14d1fa51a385f1493f56b
foreverest <dmitry.zabello@gmail.com>
2017-04-19 15:39:14 +0400

    Another attempt

--- app/src/main/java/com/example/foreverest/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/foreverest/helloandroid/MainActivity.java
@@ -1,9 +1,14 @@
-package com.example.foreverest.helloandroid;
+package/**/
+com.example.foreverest.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import //
+
+        android.support.v7.app.AppCompatActivity;
+import
+        //
+        android.os.Bundle;
+import/**/android.view.View;
+import android/**/.widget.EditText;
 
 /*
 
