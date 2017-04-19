47a025b5d35c24a2be9c957146e2a23b30843008
foreverest <dmitry.zabello@gmail.com>
2017-04-19 15:16:07 +0400

    Complicate it again

---
+++ app/src/main/java/com/example/foreverest/helloandroid/MainApplication1.java
@@ -0,0 +1,37 @@
+package
+        //
+        com.example.foreverest.helloandroid;
+
+import/**/
+//
+android/**/.app.Application;
+
+/**
+ * Created by Dmitry on 19.04.2017.
+ */
+/*
+public class MainApplication1 extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+*/
+
+public class MainApplication1 extends Application {
+    private static final String str = "@Override public void onCreate() {  super.onCreate(); }";
+
+    //@Override public void onCreate() {
+    //    super.onCreate();
+    //}
+    @Override/**/public/**/void/**/
+    //onCreate()
+    /**/onCreate() //
+    ///
+    { //
+        super.onCreate();
+        /*}*/
+        /// }
+    }
+}
